37d296097431f67877df8d04d64b8f2465d89253
Stan le Punk <stanislasiii@gmail.com>
2020-06-06 18:25:39 +0200

    compact write method

--- FUNCTION/func_anonymous_new_and_old_notation.js
+++ FUNCTION/func_anonymous_new_and_old_notation.js
@@ -2,7 +2,6 @@ const bonjour = function (prenom) {
   const message = `Bonjour, ${prenom} !`;
   return message;
 }
-
 console.log(bonjour("Thomas")); // "Bonjour, Thomas !"
 
 // new not natation for the same result it's named fat arrow function or fonction fléchée in french :)
@@ -10,5 +9,8 @@ const aurevoir = (prenom) => {
   const message = `Aurevoir, ${prenom} !`;
   return message;
 }
+console.log(aurevoir("Maurice")); // "Bonjour, Maurice !"
 
-console.log(aurevoir("Maurice")); // "Bonjour, Thomas !"
+// by default a function have a return statement so we can write like that too
+const salut = (prenom) => `Salut, ${prenom} !`;
+console.log(salut("Stan")); // "Bonjour, Stan !"
